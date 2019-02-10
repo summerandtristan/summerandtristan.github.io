c8f19267b86c8051f0fd6acb1c6f77c46275156d
monocasa <tristan.hastings.miller@gmail.com>
2019-02-09 17:48:20 -0700

    Update calendar info

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Summer and Tristan's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('May 26, 2019 16:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('May 27, 2017 2:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Under the Sun, 627 South Broadway Street, Boulder, CO 80305',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues contact Mr. Amit Roy at +91 9435021804 or +91 7086018971."
+            description: "We can't wait to see you on our big day. For any queries or issues contact Tristan Miller at (404)273-3507."
         }
     });
 
